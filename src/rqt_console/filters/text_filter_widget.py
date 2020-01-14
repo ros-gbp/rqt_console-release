e4069e5726a31d8ca63a46fb06774863d0f00b4d
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-01-14 14:58:42 -0800

    Rebase from 'upstream'

--- src/rqt_console/filters/text_filter_widget.py
+++ src/rqt_console/filters/text_filter_widget.py
@@ -38,13 +38,15 @@ from python_qt_binding.QtWidgets import QWidget
 
 
 class TextFilterWidget(QWidget):
+
     def __init__(self, parentfilter, rospack):
         """
         Widget for displaying interactive data related to text filtering.
         :param parentfilter: buddy filter were data is stored, ''TimeFilter''
         """
         super(TextFilterWidget, self).__init__()
-        ui_file = os.path.join(rospack.get_path('rqt_console'), 'resource/filters', 'text_filter_widget.ui')
+        ui_file = os.path.join(
+            rospack.get_path('rqt_console'), 'resource/filters', 'text_filter_widget.ui')
         loadUi(ui_file, self)
         self.setObjectName('TextFilterWidget')
         self._parentfilter = parentfilter  # When data is changed it is stored in the parent filter
